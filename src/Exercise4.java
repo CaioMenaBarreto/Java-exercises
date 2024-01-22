808c37e025e1bd5f7e84ef5caa438a552dd89fcd
Caio Lucas <clpmb2000@gmail.com>
2024-01-22 17:23:23 -0300

    feat: Pyramid of numbers

---
+++ src/Exercise4.java
@@ -0,0 +1,10 @@
+public class Exercise4 {
+    public static void main(String[] args) {
+        int numero = 5;
+        for(int i = 1; i <= numero; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.println(i);
+            }
+        }
+    }
+}
