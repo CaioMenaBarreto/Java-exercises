53b8d1ff6872c75fe0fd4ce61fd56a1a7b1854e3
Caio Lucas <clpmb2000@gmail.com>
2024-01-22 17:12:37 -0300

    feat: Multiplication table for x

---
+++ src/Exercise3.java
@@ -0,0 +1,9 @@
+public class Exercise3 {
+    public static void main(String[] args) {
+        int numero = 9;
+        System.out.println("Tabela de multiplicação de " + numero);
+        for(int i = 1; i < 11; i++){
+            System.out.println(numero + " X " + i + " = " + numero*i);
+        }
+    }
+}
